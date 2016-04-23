616666f3b0bdbebbe895212c7f24f29f8710d928
Tai @ Flex <dch.tai@gmail.com>
2016-04-23 16:48:48 +0100

    Get the remote name properly

--- functions/pushmaster.sh
+++ functions/pushmaster.sh
@@ -10,7 +10,7 @@ function pushmaster {
 	local backbranch=$CURBRANCH
 	switchbranch master
 	git merge "$backbranch"
-	local origin=$(git remote -v|head -n 1|grep '(push)'|egrep -o '^[a-zA-Z0-9]+')
+	local origin=$(git remote -v|grep '(push)'|egrep -o '^[a-zA-Z0-9]+')
 	if [[ -n "$origin" ]] ; then
 		infoe "Pushing your changes on $backbranch to $origin"
 		git push $origin "$backbranch"
