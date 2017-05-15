cb9e4dd948d8b931752e953d688a3211165aee31
Tai @ Flex <dch.tai@gmail.com>
2017-05-15 20:12:54 +0100

    make library call internal

--- src/githelpers/branching.sh
+++ src/githelpers/branching.sh
@@ -2,7 +2,7 @@
 function gitn_getRemote {
 	if [[ -z "${1:-}" ]]; then
 		warne Implicitly returning first remote
-		gitcall remote -v|cut -f1|sort|uniq|head -n 1
+		git remote -v|cut -f1|sort|uniq|head -n 1
 	else
 		echo "$1"
 	fi
@@ -10,7 +10,7 @@ function gitn_getRemote {
 
 function gitn_getBranch {
 	if [[ -z "${2:-}" ]]; then
-		gitcall branch --list|egrep '^\*'|cut -d' ' -f2
+		git branch --list|egrep '^\*'|cut -d' ' -f2
 	else
 		echo "$2"
 	fi
