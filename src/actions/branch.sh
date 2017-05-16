b821a3f95f7bb31ab808693e48de5a7df7b98412
Tai @ Flex <dch.tai@gmail.com>
2017-05-16 14:37:25 +0100

    add merge support

--- src/actions/branch.sh
+++ src/actions/branch.sh
@@ -8,6 +8,23 @@ function action_branch {
 		if [[ -z "$(git branch --list|egrep "^..${GITARGS_arguments[0]}$")" ]]; then
 			bopt=-b
 		fi
+
+		stashpop stash
 		gitcall checkout $bopt "${GITARGS_arguments[0]}"
+		stashpop pop
+	fi
+}
+
+function stashpop {
+	if [[ "$GSETTING_stashpop" != true ]]; then
+		return
+	fi
+
+	if [[ "$1" = stash ]]; then
+		gitcall stash
+	elif [[ "$2" = pop ]]; then
+		gitcall pop
+	else
+		faile "Invalid stashpop operation"
 	fi
 }
---
+++ src/actions/merge.sh
@@ -0,0 +1,30 @@
+function action_merge {
+	# GITARGS_* -- files, arguments
+	#merge from
+
+	local frombranch="${GITARGS_arguments[0]}"
+
+	if [[ -z "$frombranch" ]]; then faile "You need to specify a branch to pull merges from"; fi
+
+	gitcall merge "$frombranch"
+
+}
+
+# "Merge" pulls from another branch to the current branch
+# To do update, we need to switch - our original branch is then the "from" branch
+
+function action_update {
+	#merge to
+	local tobranch="${GITARGS_arguments[0]}"
+
+	if [[ -z "$tobranch" ]]; then faile "You need to specify a branch to push merges to."; fi
+
+	local frombranch="$(gitn_getBranch)"
+	if [[ -z "$frombranch" ]]; then
+		faile "Not a git repository"
+	fi
+
+	action_branch "$tobranch"   || faile "Cannot update '$tobranch'"
+	gitcall merge "$frombranch" || faile "Resolve issues"
+	action_branch "$frombranch" || faile "You may still be on '$tobranch'"
+}
