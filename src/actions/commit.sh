10532a937fe49664a02e804f05640177b2ddba6e
Tai @ Flex <dch.tai@gmail.com>
2017-05-16 22:29:33 +0100

    initial support for switching when commit from master

--- src/actions/commit.sh
+++ src/actions/commit.sh
@@ -4,7 +4,12 @@ function action_commit {
 	gitcall add "${GITARGS_files[@]}"
 
 	if [[ "$GSETTING_warnmaster" = true ]] && [[ "$(gitn_getBranch)" = master ]]; then
-		uconfirm "WARNING -- you are on master. Continue ?" || faile Abort
+		uconfirm "${CBYEL}WARNING -- you are on master. Continue ?$CDEF" || faile "Abort."
+		#uconfirm "${CBYEL}WARNING -- you are on master. Continue ?$CDEF" || {
+		#	uconfirm "Would you like to move changes to another branch ?" && {
+		#		action_branch_switchto "$(gitn_choose_branch)" || faile "Failed to switch"
+		#	} || faile Abort
+		#}
 	fi
 	
 	if [[ -z "${GITARGS_arguments[*]}" ]]; then
