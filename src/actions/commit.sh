cddfc86a5874efe62a8b1f5364e86946b404b68e
Tai @ Flex <dch.tai@gmail.com>
2017-05-16 16:27:10 +0100

    add master warnings

--- src/actions/commit.sh
+++ src/actions/commit.sh
@@ -1,5 +1,11 @@
+#%include askuser.sh
+
 function action_commit {
 	gitcall add "${GITARGS_files[@]}"
+
+	if [[ "$GSETTING_warnmaster" = true ]] && [[ "$(gitn_getBranch)" = master ]]; then
+		uconfirm "WARNING -- you are on master. Continue ?" || faile Abort
+	fi
 	
 	if [[ -z "${GITARGS_arguments[*]}" ]]; then
 		gitcall commit
--- src/settings.sh
+++ src/settings.sh
@@ -1,10 +1,14 @@
 GSETTING_gitnfile=./.gitn
 
 function settings_initialize {
-	#: ${GSETTING_=""}
-
 	: ${GSETTING_fetch=true}
 	: ${GSETTING_warnmaster=true}
+
+	: ${GSETTING_withtags=false}
+	: ${GSETTING_nofetch=false}
+	: ${GSETTING_histdiff=false}
+	: ${GSETTING_delbranch=true}
+	: ${GSETTING_force=false}
 }
 
 function settings_load {
