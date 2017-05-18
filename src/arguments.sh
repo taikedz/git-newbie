6e65467cc8d077a4081e72e8a9c2d2ee1eae8c3f
Tai <#dch tai gmail>
2017-05-18 14:48:31 +0100

    minor cleanup in arguments file

--- src/arguments.sh
+++ src/arguments.sh
@@ -1,14 +1,17 @@
 function args_processargs {
-	# Get tokens until we encounter an option or '//'
+	# This section is unfortunately ugly
+	#
+	# anything that can be done to clean up?
+	
 	if [[ -n "${GITARGS_action:-}" ]]; then
 		return
 	fi
 
 	local action_default=status
 
-	local l_GITARGS_files=
+	local l_GITARGS_files
 	GITARGS_action="$action_default"
-	local l_GITARGS_arguments=
+	local l_GITARGS_arguments
 
 	for token in "$@"; do
 		if [[ "${token:0:1}" = '-' ]]; then
@@ -17,9 +20,6 @@ function args_processargs {
 			else
 				GITARGS_action="$(args_getActionFor "$token")"
 			fi
-
-		elif [[ "$token" = '//' ]]; then
-			GITARGS_action=commit
 
 		else
 			if [[ "$GITARGS_action" = "$action_default" ]] ||
@@ -34,24 +34,30 @@ function args_processargs {
 
 	done
 
-	GITARGS_files=("${l_GITARGS_files[@]:1}")
-	GITARGS_arguments=("${l_GITARGS_arguments[@]:1}")
+	GITARGS_files=("${l_GITARGS_files[@]}")
+	GITARGS_arguments=("${l_GITARGS_arguments[@]}")
 }
 
 function arguments_dosetting {
 	case "$1" in
 	--tags)
 		export GSETTING_withtags=true ;;
+
 	--no-fetch|--nf)
 		export GSETTING_nofetch=true ;;
+
 	--historic)
 		export GSETTING_histdiff=true ;;
+
 	--delete)
 		export GSETTING_delbranch=true ;;
+
 	--force)
 		export GSETTING_force=true ;;
+
 	--no-warn-master)
 		export GSETTING_warnmaster=false ;;
+
 	--debug)
 		: ;;
 	*)
@@ -64,29 +70,41 @@ function args_getActionFor {
 		-update)
 			# merge to
 			echo "update" ;;
+
 		-merge)
 			# merge from
 			echo "merge" ;;
+
 		-m|-commit)
 			echo "commit" ;;
+
 		-l|-pull)
 			echo "pull" ;;
+
 		-s|-push)
 			echo "push" ;;
+
 		-b|-branch)
 			echo "branch" ;;
+
 		-c|-checkout)
 			echo "checkout" ;;
+
 		-g|-log)
 			echo "log" ;;
+
 		-r|-remote)
 			echo "remote" ;;
+
 		-stash)
 			echo "stash" ;;
+
 		-tags)
 			echo "tag" ;;
+
 		-release)
 			echo "release" ;;
+
 		*)
 			faile "Action $1 unknown"
 			;;
