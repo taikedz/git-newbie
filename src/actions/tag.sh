8457526dfe07a48718f1d13a0cce6f67974be635
Tai <#dch tai gmail>
2017-05-18 11:22:01 +0100

    use correct arguments

--- src/actions/tag.sh
+++ src/actions/tag.sh
@@ -1,9 +1,9 @@
 function action_tag {
 	# GITARGS_* -- files, arguments
 	
-	if [[ "${#arguments[@]}" -gt 0 ]]; then
-		local tagname="${arguments[0]}"
-		local commitmessage="${arguments[@]:1}"
+	if [[ "${#GITARGS_arguments[@]}" -gt 0 ]]; then
+		local tagname="${GITARGS_arguments[0]}"
+		local commitmessage="${GITARGS_arguments[@]:1}"
 		gitcall tag "$tagname" -m "$commitmessage"
 	else
 		gitcall tag --list
