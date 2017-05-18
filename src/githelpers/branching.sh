8f2f96df5df2e54b464bd397f55b3b5978e4f7e7
Tai <#dch tai gmail>
2017-05-18 15:11:44 +0100

    make the push and pull methods use remote as second optional

--- src/githelpers/branching.sh
+++ src/githelpers/branching.sh
@@ -2,8 +2,18 @@ function gitn_choose_branch {
 	uchoose "Which branch" "$(git branch --list 2>&1|sed -r 's/^..//')"
 }
 
+### git_getRemote and git_getBranch Usage:bbuild
+#
+# We are making the REMOTE optional, hence the slightly confusing implementation
+#
+# 	git command BRANCH
+# 	git command REMOTE BRANCH
+#
+###
+
 function gitn_getRemote {
-	if [[ -z "${1:-}" ]]; then
+	if [[ -z "${2:-}" ]]; then
+		# git command BRANCH
 		if [[ -n "${GSETTING_remote:-}" ]]; then
 			echo "$GSETTING_remote"
 		else
@@ -11,14 +21,21 @@ function gitn_getRemote {
 			git remote -v|cut -f1|sort|uniq|head -n 1
 		fi
 	else
+		# git command REMOTE BRANCH
 		echo "$1"
 	fi
 }
 
 function gitn_getBranch {
-	if [[ -z "${2:-}" ]]; then
+	if [[ -z "${1:-}" ]]; then
 		git branch --list|egrep '^\*'|cut -d' ' -f2
 	else
-		echo "$2"
+		if [[ "${2:-}" ]]; then
+			# git command BRANCH
+			echo "$1"
+		else
+			# git command REMOTE BRANCH
+			echo "$2"
+		fi
 	fi
 }
--- src/help.sh
+++ src/help.sh
@@ -19,28 +19,30 @@
 #
 # ++
 #
-# 	gitn -s [REMOTE [BRANCH]] [--tags]
+# 	gitn -s [ [REMOTE] BRANCH] [--tags]
 #
-# Push to the specified remote. If no remote is specified, then the first
-#  remote in `git remote -v` is used, or the one in $GSETTING_remote if
-#  set. See SETTINGS.
+# Push to the specified or inferred branch and remote. If no remote is
+#  specified, then the first remote in `git remote -v` is used, or the
+#  one in $GSETTING_remote if set. See SETTINGS.
 #
 #  If --tags is specified, will push tags instead of commits
 #
 # ++
 #
-# 	gitn -l [REMOTE [BRANCH] ]
+# 	gitn -l [ [REMOTE] BRANCH ]
 #
-# Pull from specified or inferred branch and remote
+# Pull from specified or inferred branch and remote. If no remote is
+#  specified, then the first remote in `git remote -v` is used, or the
+#  one in $GSETTING_remote if set. See SETTINGS.
 #
 # ++
 #
-# 	gitn -b [BRANCH] [--delete [--force]]
+# 	gitn -b [BRANCH] [--delete [--force] ]
 #
 # List branches or switch to specified branch.
 #
 # If --delete is specified, deletes the branch. Use --force to force the
-# deletion of a branch not fully merged to its upstream.
+#  deletion of a branch not fully merged to its upstream.
 #
 # ++
 #
@@ -53,7 +55,7 @@
 # 	gitn -update BRANCH
 #
 # Write the current commit state to the named branch. If the branch does
-# not exist, it is created.
+#  not exist, it is created.
 #
 # ++
 #
@@ -72,7 +74,7 @@
 # 	git -t [REMOTE [URL]]
 #
 # List remotes, list URLs of a specified remote, or set the
-# URL of a remote.
+#  URL of a remote.
 #
 ###/doc
 
