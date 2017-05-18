ee4821535800446219588453eb28a2fc617ec20f
Tai <#dch tai gmail>
2017-05-18 17:16:10 +0100

    fix tags

--- src/actions/tag.sh
+++ src/actions/tag.sh
@@ -2,7 +2,7 @@ function action_tag {
 	# GITARGS_* -- files, arguments
 	
 	if [[ "${#GITARGS_arguments[@]}" -gt 0 ]]; then
-		action_tag_writetag "${#GITARGS_arguments[@]}"
+		action_tag_writetag "${GITARGS_arguments[@]}"
 	else
 		action_tag_listtags
 	fi
